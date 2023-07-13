1f6253d48f850947357aba019a00b6257eb38532
Hamish Fletcher-Cooney <hamish.fletcher-cooney@bjss.com>
2023-07-13 16:51:26 +0100

    orginalItemsHaveHandlers

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import static java.lang.Math.max;
 import static java.lang.Math.min;
 
 class GildedRose {
@@ -11,40 +12,42 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+
             if (item.name.equals("Sulfuras, Hand of Ragnaros")){
                 continue;
             }
+
             if (item.name.contains("Backstage pass")) {
                 backstagePassHandler(item);
                 continue;
             }
 
-
-            if (!item.name.equals("Aged Brie")) {
-                if (item.quality > 0) {
-                    ChangeQuality(item,-1);
-                }
-            } else {
-                ChangeQuality(item,1);
+            if (item.name.equals("Aged Brie")){
+                agedBrieHandler(item);
+                continue;
             }
 
-            item.sellIn = item.sellIn - 1;
+            normalItemHandler(item);
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+    private void normalItemHandler(Item item) {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn >= 0) {
+            ChangeQuality(item, -1);
+        }
+        else {
+            ChangeQuality(item, -2);
+        }
+    }
+
+    private void agedBrieHandler(Item item) {
+        item.sellIn -= 1;
+        if (item.sellIn < 0 ){
+            ChangeQuality(item,2);
+        }
+        else {
+            ChangeQuality(item,1);
         }
     }
 
@@ -56,19 +59,17 @@ class GildedRose {
         }
         if (item.sellIn>=10){
             ChangeQuality(item,1);
-            return;
         }
         else if (item.sellIn>=5){
             ChangeQuality(item,2);
-            return;
         }
         else{
             ChangeQuality(item,3);
-            return;
         }
     }
 
     private void ChangeQuality(Item item, int increment){
         item.quality = min(item.quality + increment,50);
+        item.quality = max(item.quality,0);
     }
 }
