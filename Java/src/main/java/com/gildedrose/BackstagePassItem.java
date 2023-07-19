68756bf57af0141e56f56069f483af596a8f5c2a
Hamish Fletcher-Cooney <hamish.fletcher-cooney@bjss.com>
2023-07-19 11:03:00 +0100

    Final working version before factory method

---
+++ Java/src/main/java/com/gildedrose/BackstagePassItem.java
@@ -0,0 +1,15 @@
+//package com.gildedrose;
+//
+//public class BackstagePassItem implements ItemInterface{
+//
+//    private Item item;
+//
+//    public AgedItem(Item item) {
+//        this.item=item;
+//    }
+//
+//    @Override
+//    public void updateQuality(){
+////        update functionality for aged item
+//    }
+//}
